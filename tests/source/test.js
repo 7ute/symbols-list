c683ffa57d5bc5d79ed385261ed358d70a2fafeb
7ute <contact@7ute.fr>
2016-04-27 08:26:51 +0200

    Added test files

---
+++ tests/source/test.js
@@ -0,0 +1,12 @@
+
+// ! commentaire
+/* ! commentaire_multi */
+class Class
+function Function1(){}
+function Function2( arg1, arg2 ){}
+private function Function3(){}
+.controller('controller', function(){});
+$scope.method = function(){};
+.filter('filter', function(){});
+.config(function(){});
+.run(function(){});
