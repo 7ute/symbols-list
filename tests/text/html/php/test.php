88832f95a25a27c7d7a4211794418607834f1f44
7ute <contact@7ute.fr>
2016-05-31 21:20:01 +0200

    Added static method and fixed symbol jump above functions

---
+++ tests/text/html/php/test.php
@@ -0,0 +1,12 @@
+<?php
+class Classe
+{
+    public static function methode_statique(){ }
+    public function methode(){ }
+}
+
+function &statique() {...}
+function & autre_statique() {...}
+
+// ! commentaire
+/* ! Commentaire multiligne */
