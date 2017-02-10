6a0930cd0817b7d7a7650860726ea4b557ae303f
Marco Buchholz <marco.buchholz@otrs.com>
2017-02-10 18:12:11 +0100

    Removed not neede license header.

--- tests/source/perl.pm
+++ tests/source/perl.pm
@@ -1,11 +1,3 @@
-# --
-# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
-# --
-# This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (AGPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
-# --
-
 package My::Test::Package;
 
 use strict;
