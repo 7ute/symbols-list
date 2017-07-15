fded88ffc01c2d5e7d3994fc9046df751d166c29
Marco Buchholz <mabu@debian-admin.de>
2017-07-15 22:55:53 +0200

    Improved es6method regular expression.

--- tests/source/test.js
+++ tests/source/test.js
@@ -2,18 +2,53 @@
 // ! commentaire
 /* ! commentaire_multi */
 class Class
-function Function1(){}
-function Function2( arg1, arg2 ){}
-private function Function3(){}
-.controller('controller', function(){});
-$scope.method = function(){};
-.filter('filter', function(){});
-.config(function(){});
-.run(function(){});
+
+function Function1() {
+
+}
+
+function Function2( arg1, arg2 ) {
+
+}
+
+private function Function3() {
+
+}
+
+.controller('controller', function() {
+
+});
+
+$scope.method = function() {
+
+};
+
+.filter('filter', function() {
+
+});
+
+.config(function() {
+
+});
+
+.run(function() {
+
+});
 
 class SomeClass {
-    set foo(value) {}
-    get foo() {}
-    set bar( value ) {}
-    get bar() {}
+    set foo(value) {
+
+    }
+
+    get foo() {
+
+    }
+
+    set bar( value ) {
+
+    }
+
+    get bar() {
+
+    }
 }
