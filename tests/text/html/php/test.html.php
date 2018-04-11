921b908744be8e81cd1cdad91fbd3145fdd9bd3c
7ute <contact@7ute.fr>
2018-04-11 23:43:25 +0200

    Added simple HTML subscript test (PHP)

---
+++ tests/text/html/php/test.html.php
@@ -0,0 +1,11 @@
+<?php include('dummy-view') ?>
+
+<section aria-label="first-section">
+    <h1>{{ $some_var }}</h1>
+    <h2>Test</h2>
+</section>
+
+<section>
+    <!-- ! Comment -->
+    <script src="<?= asset('script.js') ?>" charset="utf-8"></script>
+</section>
